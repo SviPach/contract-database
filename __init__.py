fdf23f83e562dff01d76964c1b3a43abe1c3e551
Sviatoslav <s_balema@utb.cz>
2024-04-21 18:03:23 +0200

    v3

--- __init__.py
+++ __init__.py
@@ -1,10 +1,20 @@
-"""Database cpntracts package.
+"""Database contracts package.
+
+Database of company's contracts.
+Contracts include:
+    1. Client's name and surname.
+    2. Contract's ID.
+    3. Client's ID.
+    4. Contract's start date.
+    5. Contract's end date.
+    6. Client's paid amount.
 
 .. include:: ../README.md
 """
 
 import class_ES  # noqa: F401
 import value_check  # noqa: F401
+import main # noqa: F401
 
 
 # __all__ = [stat_ukaz]
